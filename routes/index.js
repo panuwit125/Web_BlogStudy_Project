aba5b508a156c71b956f96fde5387a44cf7e90fe
panuwit125 <panuwit.bas@gmail.com>
2020-04-04 20:14:03 +0700

    ๊Update View

--- routes/index.js
+++ routes/index.js
@@ -1,46 +1,54 @@
 module.exports = {
   indexRouterPage: (req,res) => {
-    if (req.session.loggedin){
-      let user_name = req.session.username;
-      let queryviewblog = "SELECT * FROM blog_studentandteacher";
-      db.query(queryviewblog,(err,result) => {
-        if (err) {
-          return res.status(500).send(err);
-        }
-        let queryblogadmin = "SELECT * FROM blog_staff"
-        db.query(queryblogadmin,(err,resultblogadmin) => {
+    let query = "SELECT * FROM create_classroom";
+    db.query(query,(err,resultclass) => {
+      if (err) {
+        return res.status(500).send(err);
+      }
+      if (req.session.loggedin){
+        let user_name = req.session.username;
+        let queryviewblog = "SELECT * FROM blog_studentandteacher";
+        db.query(queryviewblog,(err,result) => {
           if (err) {
             return res.status(500).send(err);
           }
-          res.render('index.ejs',{
-            results:req.session.loggedin,
-            name:user_name[0].Username,
-            money:user_name[0].Money,
-            resultblog: result,
-            resultblogadmin: resultblogadmin
+          let queryblogadmin = "SELECT * FROM blog_staff"
+          db.query(queryblogadmin,(err,resultblogadmin) => {
+            if (err) {
+              return res.status(500).send(err);
+            }
+            res.render('index.ejs',{
+              results:req.session.loggedin,
+              name:user_name[0].Username,
+              money:user_name[0].Money,
+              resultblog: result,
+              resultblogadmin: resultblogadmin,
+              resultclass: resultclass
+            });
           });
+          
         });
-        
-      });
-    } else {
-      let queryviewblog = "SELECT * FROM blog_studentandteacher";
-      db.query(queryviewblog,(err,result) => {
-        if (err) {
-          return res.status(500).send(err);
-        }
-        let queryblogadmin = "SELECT * FROM blog_staff"
-        db.query(queryblogadmin,(err,resultblogadmin) => {
+      } else {
+        let queryviewblog = "SELECT * FROM blog_studentandteacher";
+        db.query(queryviewblog,(err,result) => {
           if (err) {
             return res.status(500).send(err);
           }
-          res.render('index.ejs',{
-            resultblog: result,
-            resultblogadmin: resultblogadmin,
-            results:""
+          let queryblogadmin = "SELECT * FROM blog_staff"
+          db.query(queryblogadmin,(err,resultblogadmin) => {
+            if (err) {
+              return res.status(500).send(err);
+            }
+            res.render('index.ejs',{
+              resultblog: result,
+              resultblogadmin: resultblogadmin,
+              resultclass: resultclass,
+              results:""
+            });
           });
+          
         });
-        
-      });
-    }
+      }
+    });
   }
 }
